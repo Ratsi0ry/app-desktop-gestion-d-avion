011dc4c967282f8b3637790fbd63e9fac2a93f0f
Ratsi0ry <tsiorymonja411@gmail.com>
2026-07-31 14:32:18 +0300

    launch page en cours

---
+++ Views/Pages/WelcomeView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Gestion_avion.Views.Pages;
+
+public partial class WelcomeView : UserControl
+{
+    public WelcomeView()
+    {
+        InitializeComponent();
+    }
+}
