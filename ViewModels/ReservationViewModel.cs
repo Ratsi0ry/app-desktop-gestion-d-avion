7e5ee87bc26f22af897fd442a13d96a078280bd6
Ratsi0ry <tsiorymonja411@gmail.com>
2026-08-02 13:02:56 +0300

    ui ajout layout reservation

--- ViewModels/ReservationViewModel.cs
+++ ViewModels/ReservationViewModel.cs
@@ -6,10 +6,21 @@ namespace Gestion_avion.ViewModels;
 public partial class ReservationViewModel: ViewModelBase
 {
     [ObservableProperty]
-    private string _villeDisponibles, _villeDepart, _villeArrivee, _dateVol, _heureVol, _nom, _prenom;
+    private string _villeDisponibles, _villeDepart, _villeArrivee, _dateVol, _heureVol, _nom, _prenom,
+                    _idPasseport, _categoriePersonne, _classeAvion, _compagnieAerienne;
 
+                    
     [ObservableProperty]
-    private short _age;
+    private short _age, _nbPersonne, _sejour;
+
+    //affichage du label sejour
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(IsSejourVisible))]
+    private string _typeVol;
+
+    public bool IsSejourVisible => TypeVol == "Aller-retour";
+
+    
     public ReservationViewModel()
     {
         VilleDepart = "";
@@ -20,5 +31,12 @@ public partial class ReservationViewModel: ViewModelBase
         Nom = "";
         Prenom = "";
         Age = 0;
+        IdPasseport = "";
+        NbPersonne = 0;
+        CategoriePersonne = "";
+        ClasseAvion = "";
+        TypeVol = "";
+        CompagnieAerienne = "";
+        Sejour = 0;
     }
 }
