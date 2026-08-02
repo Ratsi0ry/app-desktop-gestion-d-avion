5b10959fa47f569ce4218548a729b7549add8967
xXmemineXx <aferdino01@gmail.com>
2026-08-02 13:38:13 +0300

    transparent

---
+++ Views/SignUpView.axaml.cs
@@ -0,0 +1,34 @@
+using Avalonia.Controls;
+using Avalonia.Platform.Storage;
+
+namespace Gestion_avion.Views.Pages;
+
+public partial class SignUpView : UserControl
+{
+    public SignUpView()
+    {
+        InitializeComponent();
+        new_user.IsVisible = false;
+        other_accounts.IsVisible = false;
+    }
+
+    private void other_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        new_user.IsVisible = false;
+        other_accounts.IsVisible = true;
+        picker.IsVisible = false;
+    }
+    private void register_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        new_user.IsVisible = true;
+        other_accounts.IsVisible = false;
+        picker.IsVisible = false;
+    }
+
+    private void pick_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        new_user.IsVisible = false;
+        other_accounts.IsVisible = false;
+        picker.IsVisible = true;
+    }
+}
