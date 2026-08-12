017dd5a90cec3901546e87f1fef0d4f9f80e097b
xXmemineXx <aferdino01@gmail.com>
2026-08-12 08:55:08 +0300

    flight-update

--- ViewModels/FlightCardViewModel.cs
+++ ViewModels/FlightCardViewModel.cs
@@ -11,12 +11,28 @@ public partial class FlightCardViewModel : ViewModelBase
     [ObservableProperty]
     public string _plane, _company, _portA, _portB, _depart;
 
-    public FlightCardViewModel(string plane, string company, string portA, string portB, string depart)
+    private readonly Action<FlightCardViewModel> _delayed;
+    private readonly Action<FlightCardViewModel> _delete;
+    public FlightCardViewModel(string plane, string company, string portA, string portB, string depart, Action<FlightCardViewModel> delay, Action<FlightCardViewModel> remove)
     {
         _plane = plane;
         _company = company;
         _portA = portA;
         _portB = portB;
         _depart = depart;
+        _delayed = delay;
+        _delete = remove;
+    }
+
+    [RelayCommand]
+    public void Delay()
+    {
+        _delayed?.Invoke(this);
+    }
+
+    [RelayCommand]
+    public void Delete()
+    {
+        _delete?.Invoke(this);
     }
 }
--- ViewModels/VolsViewModel.cs
+++ ViewModels/VolsViewModel.cs
@@ -2,68 +2,119 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System.Collections.ObjectModel;
 using System.Collections.Generic;
+using System;
 
 namespace Gestion_avion.ViewModels;
 
-public class VolsViewModel : ViewModelBase
+public partial class VolsViewModel : ViewModelBase
 {
-    Dictionary<string, string>[] allFlights = new Dictionary<string, string>[]
+    [ObservableProperty]
+    private string? _plane_, _d, _a, _conf;
+
+    [ObservableProperty]
+    private bool _sure;
+
+    [ObservableProperty]
+    private DateTimeOffset? _departureDate, _currentView;
+
+    [ObservableProperty]
+    private TimeSpan? _departureTime;
+
+    [ObservableProperty]
+    private ObservableCollection<string> availablePorts = new ()
+    {
+        "Antananarivo",
+        "Toamasina",
+        "Allemagne",
+        "France",
+        "Fianarantsoa",
+        "Sambava"
+    };
+
+    [ObservableProperty]
+    private ObservableCollection<string> availablePlanes = new()
+    {
+        "A1",
+        "A2",
+        "A3",
+        "A4",
+        "A5"
+    };
+
+    class Flight
+    {
+        public string Plane, Company, PortA, PortB;
+        public DateTime Departure;
+
+        public Flight(string plane, string company, DateTime departure, string A, string B)
         {
-            new Dictionary<string, string>
-            {
-                { "Plane", "Arry" },
-                { "Company", "azaa" },
-                { "PortA", "Tananarive"},
-                { "PortB", "Toamasina"},
-                { "Depart", "12h"}
-            },
-            new Dictionary<string, string>
-            {
-                { "Plane", "Sora" },
-                { "Company", "sryi" },
-                { "PortA", "Tananarive"},
-                { "PortB", "Toamasina"},
-                { "Depart", "12h"}
-            },
-            new Dictionary<string, string>
-            {
-                { "Plane", "Abata" },
-                { "Company", "akou" },
-                { "PortA", "Tananarive"},
-                { "PortB", "Toamasina"},
-                { "Depart", "12h"}
-            },
-            new Dictionary<string, string>
-            {
-                { "Plane", "Bibo" },
-                { "Company", "kaor" },
-                { "PortA", "Tananarive"},
-                { "PortB", "Toamasina"},
-                { "Depart", "12h"}
-            },
-            new Dictionary<string, string>
-            {
-                { "Plane", "kasp" },
-                { "Company", "tsar" },
-                { "PortA", "Tananarive"},
-                { "PortB", "Toamasina"},
-                { "Depart", "12h"}
-            }
-        };
+            Plane = plane;
+            Company = company;
+            Departure = departure;
+            PortA = A;
+            PortB = B;
+        }
+    }
+
+    List<Flight> allFlights = [
+      new Flight("A1", "azaa", DateTime.Now,"Antananarivo", "Toamasina"),
+      new Flight("A2", "azaa", DateTime.Now,"France", "Toamasina"),
+      new Flight("A3", "azaa", DateTime.Now,"Antananarivo", "Allemagne"),
+      new Flight("A4", "azaa", DateTime.Now,"Sambava", "Toamasina"),
+      new Flight("A5", "azaa", DateTime.Now,"Fianarantsoa", "Toamasina"),
+    ];
 
     public ObservableCollection<FlightCardViewModel> FlightList{get; set;}
     public VolsViewModel()
     {
         FlightList = new ObservableCollection<FlightCardViewModel>();
-        for (int i = 0; i < allFlights.Length; i++)
+        Conf = "confirmer l'ajout";
+        Sure = false;
+        CurrentView = (DateTimeOffset)DateTime.Now;
+        foreach (Flight f in allFlights)
         {
-            FlightList.Add(new FlightCardViewModel( allFlights[i]["Plane"],
-                                                    allFlights[i]["Company"],
-                                                    allFlights[i]["PortA"],
-                                                    allFlights[i]["PortB"],
-                                                    allFlights[i]["Depart"]
+            FlightList.Add(new FlightCardViewModel( f.Plane,
+                                                    f.Company,
+                                                    f.PortA,
+                                                    f.PortB,
+                                                    f.Departure.ToString("HH:mm"),
+                                                    OnDelay,
+                                                    OnDelete
                                                     ));
         }
     }
+
+    private void OnDelay(FlightCardViewModel currflight)
+    {
+        Plane_ = currflight.Plane;
+        foreach (Flight f in allFlights)
+        {
+            if (currflight.Plane == f.Plane)
+            {
+                DepartureTime = f.Departure.TimeOfDay;
+                DepartureDate = (DateTimeOffset)f.Departure;
+                D = f.PortA;
+                A = f.PortB;
+                Conf = "Confirmer les modifications";
+            }
+        }
+    }
+
+    private void OnDelete(FlightCardViewModel currFlight)
+    {
+        Sure = true;
+    }
     
+    [RelayCommand]
+    private void Reset()
+    {
+        A = null;
+        Plane_ = null;
+        D = null;
+        DepartureDate = null;
+        DepartureTime = null;
+        Conf = "confirmer l'ajout";
+        Sure = false;
+    }
+
 }
