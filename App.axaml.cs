6cd6531cab52a621c3a3eb276f51a67878a9dd17
Ratsi0ry <tsiorymonja411@gmail.com>
2026-08-16 15:52:34 +0300

    Revert "ajout tarif & paiement, debug modification"

--- App.axaml.cs
+++ App.axaml.cs
@@ -3,14 +3,13 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using Gestion_avion.ViewModels;
 using Gestion_avion.Views;
-using Gestion_avion.state;
+
+
 
 namespace Gestion_avion;
 
 public partial class App : Application
 {
-    private readonly AppState _appState = new AppState();
-
     //activation du font awesome
     public override void Initialize()
     {
@@ -23,7 +22,7 @@ public partial class App : Application
         {
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainViewModel(_appState),
+                DataContext = new MainViewModel(),
             };
         }
 
--- ViewModels/AppState.cs
+++
@@ -1,8 +0,0 @@
-using back.Models;
-
-namespace Gestion_avion.state;
-
-public class AppState
-{
-	public Compagnie? currentCompanie {get; set;}
-}
