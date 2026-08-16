ddc8b6f935f903c5095148781201c4513c032c9b
Mimy-3600 <vahatrahasambarana@gmail.com>
2026-08-16 19:33:56 +0300

    Small fix

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -13,7 +13,6 @@ public partial class MainViewModel : ViewModelBase
 
     private readonly AppState _appState;
 
-    //interface handling
     [ObservableProperty]
     private ViewModelBase? _currentInterface;
 
@@ -30,46 +29,44 @@ public partial class MainViewModel : ViewModelBase
     }
 
     private async Task InitializeAppAsync()
-{
-    LoadingMessage = "Connexion à la base de données...";
-    await Task.Delay(1000);
-
-    LoadingMessage = "Chargement de la liste des vols...";
-    await Task.Delay(1000);
-
-    LoadingMessage = "Préparation de l'interface...";
-    await Task.Delay(500);
-
-    var signUpVM = new SignUpViewModel(_appState);
-    CurrentInterface = signUpVM;
-    IsLoading = false;
-
-    
-    var logTask = new TaskCompletionSource<bool>();
-
-    // 3. S'abonner au changement de propriété
-    System.ComponentModel.PropertyChangedEventHandler handler = null!;
-    handler = (s, e) =>
     {
-        // Remplacer "IsLogged" par le nom exact de votre propriété dans SignUpViewModel
-        if (e.PropertyName == nameof(SignUpViewModel.IsLogged) && signUpVM.IsLogged)
+        LoadingMessage = "Connexion à la base de données...";
+        await Task.Delay(1000);
+
+        LoadingMessage = "Chargement de la liste des vols...";
+        await Task.Delay(1000);
+
+        LoadingMessage = "Préparation de l'interface...";
+        await Task.Delay(500);
+
+        var signUpVM = new SignUpViewModel(_appState);
+        CurrentInterface = signUpVM;
+        IsLoading = false;
+
+        
+        var logTask = new TaskCompletionSource<bool>();
+
+        System.ComponentModel.PropertyChangedEventHandler handler = null!;
+        handler = (s, e) =>
         {
-            signUpVM.PropertyChanged -= handler;
-            logTask.SetResult(true);
-        }
-    };
+            if (e.PropertyName == nameof(SignUpViewModel.IsLogged) && signUpVM.IsLogged)
+            {
+                signUpVM.PropertyChanged -= handler;
+                logTask.SetResult(true);
+            }
+        };
 
-    signUpVM.PropertyChanged += handler;
+        signUpVM.PropertyChanged += handler;
 
-    await logTask.Task;
+        await logTask.Task;
 
-    IsLoading = true;
+        IsLoading = true;
 
-    LoadingMessage = "Préparation de l'interface...";
-    await Task.Delay(500);
+        LoadingMessage = "Préparation de l'interface...";
+        await Task.Delay(500);
 
-    CurrentInterface = new InterfaceViewModel(_appState);
-    IsLoading = false;
-}
+        CurrentInterface = new InterfaceViewModel(_appState);
+        IsLoading = false;
+    }
 
 }
